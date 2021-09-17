29fb2ef04a32d5c43e8d2bfe8a5cd649eaf59083
unknown <sanninm>
2021-09-17 18:05:14 +0200

    Word Blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    // Only change code below this line
+     var myNoun = "dog";
+     var myVerb = "run";
+     var myAdjective = "big";
+     var myAdverb = "quickly";
+     wordBlanks =  myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb + ".";
+     
+    // Only change code below this line 
+    
+    return wordBlanks;
+        
+    }
+    
+    console.log(main());
+    module.exports = main;
