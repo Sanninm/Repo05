8bcc34ea417387aa800e12f392539cf1546fc2e7
Sanninm <mudzelet.sanin@gmail.com>
2021-09-18 10:40:54 +0200

    Quoting Strings with Single Quotes

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,6 +1,6 @@
 function main(){
     // Only change code below this line
-    var myLink = '<a herf="http://www.example.com" target="_blank">Link<a/>'
+    var myLink = '<a href="http://www.example.com" target="_blank">Link<a/>'
     
     // Only change code below this line 
     
