11f9fc7a645b67f8d93c39b74567cbe652c6a726
Sanninm <mudzelet.sanin@gmail.com>
2021-09-18 10:38:56 +0200

    Quoting Strings with Single Quotes

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // Only change code below this line
+    var myLink = '<a herf="http://www.example.com" target="_blank">Link<a/>'
+    
+    // Only change code below this line 
+    
+    return {
+        myLink,
+        
+    };
+    
+    }
+    
+    console.log(main());
+    module.exports = main;
